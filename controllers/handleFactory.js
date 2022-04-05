aed6823c8b2e3f3f01a9f6637e38e2a690eb1cf0
zedxog <zubydev2012@gmail.com>
2022-04-05 21:19:52 +0100

    initial commit

---
+++ controllers/handleFactory.js
@@ -0,0 +1,61 @@
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+
+exports.deleteOne = Model =>
+    catchAsync(async (req, res, next) => {
+        const doc = await Model.findByIdAndDelete(req.params.Id);
+
+        if (!doc) {
+            return next(new AppError("No document found with that Id", 404));
+        }
+        res.status(204).json({
+            status: "successful",
+            data: null
+        });
+    });
+
+exports.updateOne = Model => catchAsync(async (req, res, next) => {
+        const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if(!doc) {
+            return next(new AppError("No document found with that Id", 404));
+        }
+        res.status(201).json({
+            status: "successful",
+            data: {
+                data: doc
+            }
+        });
+});
+
+exports.createOne = Model => catchAsync( async (req, res, next) => {
+   const doc = await Model.create(req.body);
+
+    if(!doc) {
+        return next(new AppError("No document found with that Id", 404));
+    }
+   res.status(201).json({
+       status: "success",
+       data: {
+           data: doc
+       }
+   })
+});
+
+exports.getOne = (Model, popOptions) => catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc){
+        return next(new AppError("no document found with that Id", 404))
+    }
+    res.status(200).json({
+        status: "success",
+        data: {
+            data: doc
+        }
+    })
+});
