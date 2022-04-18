ad0c765491a9c903bc11420b46577ad2bf4a5f3a
viktorvoltz <vctrchinyeaka@gmail.com>
2022-04-18 11:37:22 +0100

    image route

---
+++ routes/imageRoute.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const multer = require('../utils/multer');
+const imageUpload = require('../controllers/imageController');
+const router = express.Router();
+
+//allow multiple image uploads
+router.post("/upload", multer.array('image'), imageUpload);
