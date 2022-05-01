1bb1b26f307e67bf677337f38272f8ddea8fde3f
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-01 20:37:05 +0100

    bug fix

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,37 +1,36 @@
-const catchAsync = require("../utils/catchAsync");
-const cloudinary = require("../utils/config");
+const catchAsync = require("../utils/catchAsync.js");
+const cloudinary = require("../utils/config.js");
 const fs = require('fs');
 
 
-exports.imageUpload = catchAsync(
-    async(req, res) => {
+exports.imageUpload =
+  async (req, res) => {
 
-        const uploader = async (path) => await cloudinary.uploads(path, 'images');
-      
-        if (req.method == 'POST'){
-          const urls = []
-      
-          const files = req.files
-      
-          for (const file of files){
-            const {path} = file
-      
-            const newPath = await uploader(path)
-      
-            urls.push(newPath)
-      
-            fs.unlinkSync(path)
-          }
-      
-          res.status(200).json({
-            message: 'images Uploaded Successfully',
-            data: urls
-          })
-          console.log("success", urls);
-        }else{
-          res.status(405).json({
-            err: "error uploading image"
-          })
-        }
+    const uploader = async (path) => await cloudinary.uploads(path, 'images');
+
+    if (req.method == 'POST') {
+      const urls = []
+
+      const files = req.files
+
+      for (const file of files) {
+        const { path } = file
+
+        const newPath = await uploader(path)
+
+        urls.push(newPath)
+
+        fs.unlinkSync(path)
       }
-);
+
+      res.status(200).json({
+        message: 'images Uploaded Successfully',
+        data: urls
+      })
+      console.log("success", urls);
+    } else {
+      res.status(405).json({
+        err: "error uploading image"
+      })
+    }
+  }
