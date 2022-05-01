250c18efc634970af13ffd27ea71a4ec5005dbf1
viktorvoltz <vctrchinyeaka@gmail.com>
2022-05-01 20:36:14 +0100

    multer bug fix

--- controllers/googleAuth.js
+++ controllers/googleAuth.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const User = require("../models/userModel");
 var GoogleStrategy = require('passport-google-oauth2').Strategy;
+require('dotenv').config()
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
@@ -13,7 +14,7 @@ passport.use(new GoogleStrategy({
     callbackURL: `/api/v1/user/google/callback`,
     passReqToCallback: true
 },
-    function (request, accessToken, refreshToken, profile, done) {
+    async function (request, accessToken, refreshToken, profile, done) {
         //User.findOrCreate({ googleId: profile.id }, function (err, user) {
         //    return done(null, user);
         //});
